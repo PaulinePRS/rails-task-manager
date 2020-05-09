9f60d600eee194ff954a8b4c2288906401100c3b
pauline paris <paulineparisparis@hotmail.fr>
2020-05-09 15:37:09 +0200

    delete and finish

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   post 'tasks', to: 'tasks#create'
   get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
   patch 'tasks/:id', to: 'tasks#update'
+  delete 'tasks/:id', to: 'tasks#destroy', as: :destroy
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
